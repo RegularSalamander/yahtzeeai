ffcb75d115318d0a0c4b92d3c9b447db6f1b7523
Cameron Kelly <cameron.kelly@utdallas.edu>
2025-02-16 22:49:49 -0600

    score calculations

--- script.js
+++ script.js
@@ -32,7 +32,6 @@ function randint(min, max) {
 }
 
 function clickDie(obj) {
-    console.log(obj.id);
     let rolling = obj.getAttribute("data-rolling") == "true"
     obj.setAttribute("data-rolling", !rolling);
 }
@@ -66,5 +65,57 @@ function setDiceImgs() {
         for(let i = 1; i <= 5; i++) {
             $(`player${player}die${i}`).firstElementChild.setAttribute("src", `images/${diceStates[player-1][i-1]}.png`);
         }
+    }
+}
+
+function scoreDice(player) {
+    let dice = diceStates[player];
+    let diceTypes = [0, 0, 0, 0, 0, 0];
+    for(let i = 0; i < 5; i++)
+        diceTypes[dice[i] - 1]++;
+    let diceSum = dice.reduce((x,y) => x+y);
+
+    let scoreOptions = {};
+    
+    scoreOptions["ones"] = diceTypes[0];
+    scoreOptions["twos"] = diceTypes[1]*2;
+    scoreOptions["threes"] = diceTypes[2]*3;
+    scoreOptions["fours"] = diceTypes[3]*4;
+    scoreOptions["fives"] = diceTypes[4]*5;
+    scoreOptions["sixes"] = diceTypes[5]*6;
+
+    scoreOptions["threeofakind"] = 0;
+    if(Math.max(...diceTypes) >= 3)
+        scoreOptions["threeofakind"] = diceSum;
+
+    scoreOptions["fourofakind"] = 0;
+    if(Math.max(...diceTypes) >= 4)
+        scoreOptions["fourofakind"] = diceSum;
+
+    scoreOptions["fullhouse"] = 0;
+    if(diceTypes.indexOf(3) >= 0 && diceTypes.indexOf(2) >= 0)
+        scoreOptions["fullhouse"] = 25;
+
+    scoreOptions["smallstraight"] = 0;
+    if(
+        diceTypes[0] > 0 && diceTypes[1] > 0 && diceTypes[2] > 0 && diceTypes[3] > 0 ||
+        diceTypes[1] > 0 && diceTypes[2] > 0 && diceTypes[3] > 0 && diceTypes[4] > 0 ||
+        diceTypes[2] > 0 && diceTypes[3] > 0 && diceTypes[4] > 0 && diceTypes[5] > 0
+    ) scoreOptions["smallstraight"] = 30;
+
+    scoreOptions["largestraight"] = 0;
+    if(
+        diceTypes[0] > 0 && diceTypes[1] > 0 && diceTypes[2] > 0 && diceTypes[3] > 0 && diceTypes[4] > 0 ||
+        diceTypes[1] > 0 && diceTypes[2] > 0 && diceTypes[3] > 0 && diceTypes[4] > 0 && diceTypes[5] > 0
+    ) scoreOptions["largestraight"] = 40;
+
+    scoreOptions["yahtzee"] = 0;
+    if(Math.max(...diceTypes) >= 5)
+        scoreOptions["yahtzee"] = 50;
+
+    scoreOptions["chance"] = diceSum;
+
+    for(let i in scoreOptions) {
+        $(`player${player}score${i}`).innerHTML = scoreOptions[i];
     }
 }
